aff4e762aefaeea8d26ce540c50cbef1cd816873
Loris Benedetto <loris.benedetto@pallino.it>
2019-07-26 10:00:28 +0200

    Applicazione di FE

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,135 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+    <div id="app">
+        <div v-if="!isLoading">
+            <div class="date-information">
+                <h5>Sol {{ informations.sol }}</h5>
+                <h6> {{ informations.LastUTC }}</h6>
+            </div>
+
+            <div class="temperature-information">
+                <h1>{{ informations.av }}°{{ indexOfTemperature }}</h1>
+                <h5>max {{ informations.mx }}°{{ indexOfTemperature }}</h5>
+                <h5>min {{ informations.mn }}°{{ indexOfTemperature }}</h5>
+            </div>
+
+            <div class="button-information">
+                <b-button @click="getInformation" class="button-refresh">Refresh</b-button>
+                <b-button @click="changeInformation" class="button-change">C°/F°</b-button>
+            </div>
+        </div>
+        <div v-else>
+            <div>
+                <h1>Waiting...</h1>
+            </div>
+        </div>
+    </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld'
+    import 'bootstrap/dist/css/bootstrap.css'
+    import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        data() {
+            return {
+                informations: {},
+                indexOfTemperature: 'C',
+                isLoading: false,
+            }
+        },
+        methods: {
+            getInformation() {
+                this.isLoading = true
+                this.$axios.get('http://localhost:8000/api/mars-informations', {
+                    headers: {
+                        'Access-Control-Allow-Origin': '*',
+                    }
+                })
+                    .then(response => {
+                        this.informations = response.data
+                        this.isLoading = false
+                    })
+                    .catch(e => {
+                        console.log(e)
+                        this.isLoading = false
+                    })
+            },
+            changeInformation() {
+                this.isLoading = true
+
+                this.$axios.post('http://localhost:8000/api/change-index',
+                    {
+                        indexOfTemperature: this.indexOfTemperature,
+                        temperature: this.informations.av
+                    }
+                )
+                    .then(response => {
+                        this.informations.av = response.data.value
+                    })
+                    .catch(e => {
+                        console.log(e)
+                    })
+
+                this.$axios.post('http://localhost:8000/api/change-index',
+                    {
+                        indexOfTemperature: this.indexOfTemperature,
+                        temperature: this.informations.mx
+                    }
+                )
+                    .then(response => {
+                        this.informations.mx = response.data.value
+                    })
+                    .catch(e => {
+                        console.log(e)
+                    })
+
+                this.$axios.post('http://localhost:8000/api/change-index',
+                    {
+                        indexOfTemperature: this.indexOfTemperature,
+                        temperature: this.informations.mn
+                    }
+                )
+                    .then(response => {
+                        this.informations.mn = response.data.value
+                    })
+                    .catch(e => {
+                        console.log(e)
+                    })
+
+                this.indexOfTemperature = this.indexOfTemperature === 'C' ? 'F' : 'C'
+
+                this.isLoading = false
+            }
+        },
+        created() {
+            this.getInformation()
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+
+    .temperature-information {
+        margin-top: 150px;
+    }
+
+    .button-information {
+        margin-top: 150px;
+    }
+
+    .button-refresh {
+        background-color: blue !important;
+    }
+
+    .button-change {
+        background-color: green !important;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,18 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import BootstrapVue from 'bootstrap-vue'
+
+import axios from 'axios'
+
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios.create()
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    components: {App},
+    template: '<App/>',
 })
